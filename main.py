631ba78902399f9fee9bd1fa3247457887de0edf
Hasnat4763 <abulhasnat5737@gmail.com>
2026-01-01 00:16:26 +0600

    Changed so it should wish anytime after 12:00 am

--- main.py
+++ main.py
@@ -398,7 +398,7 @@ def find_and_send_wishes():
             else:
                 user_now = datetime.now(pytz.utc)
             
-            if (user_now.hour >= 0 and user_now. hour < 1) and user_now.day == day and user_now.month == month:
+            if user_now.hour >= 0 and user_now.day == day and user_now.month == month:
                 famous_person = get_random_famous(month, day)
                 famous_text = format_birthday(famous_person)
                 dm_message = f"""🎉🎂 *Happy Birthday! * 🎈🎁
